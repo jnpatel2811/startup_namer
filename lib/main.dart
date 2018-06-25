768a3c4e410ef6ffb1d83e05c654fda4a58cac25
Jay Patel <jnpatel2811@gmail.com>
2018-06-25 19:46:55 +0530

    generating and showing startup names in infinite list view.

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new RandomWords(),
-        ),
+      title: 'Startup Name Generator',
+      home: new RandomWordsWidget(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWordsWidget> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row.
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row in the ListView for odd items
+          if (i.isOdd) {
+            return new Divider();
+          }
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          // getting the actual index of name as list also includes dividers
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word pairings...
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
-  }
-}
-
-class RandomWords extends StatefulWidget {
+class RandomWordsWidget extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
 }
